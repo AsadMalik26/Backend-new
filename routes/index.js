d78666215426cad34914b393885a988ab342fcc7
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-07-26 17:29:06 +0500

    added cookie route for testing cookie

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,19 @@ router.get("/cart", function (req, res, next) {
   res.send(cart);
 });
 
+router.get("/cookie", function (req, res) {
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined) {
+    // no: set a new cookie
+    var randomNumber = Math.random().toString();
+    randomNumber = randomNumber.substring(2, randomNumber.length);
+    res.cookie("cookieName", randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log("cookie created successfully");
+  } else {
+    // yes, cookie was already present
+    console.log("cookie exists", cookie);
+  }
+  res.send(["Check Cookie mate", cookie]);
+});
+
 module.exports = router;
